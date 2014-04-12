481ad6db0b1ccfe777f32c3cb77af7002ca8fda7
Piet van Dongen <piet@studio-donder.nl>
2014-04-12 22:19:56 +0200

    First complete version of font

--- GruntFile.js
+++ GruntFile.js
@@ -1,13 +1,30 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            clean: [
+                'build/fonts',
+                'build/styles'
+            ]
+        },
         webfont: {
             icons: {
                 src: 'icons/*.svg',
-                dest: 'build/fonts'
+                dest: 'build/fonts',
+                destCss: 'build/styles',
+                options: {
+                    font: 'iCheckMovies',
+                    hashes: false,
+                    syntax: 'bootstrap',
+                    template: 'templates/less',
+                    stylesheet: 'less',
+                    relativeFontPath: '@{icm-icon-font-path}@{icm-icon-font-name}',
+                    htmlDemo: false
+                }
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-webfont');
 };
